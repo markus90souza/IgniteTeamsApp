260ddd0e65f2c8463ea0d58d9b709db3cd7adfe6
markus90souza <markus90souza@gmail.com>
2022-09-19 13:18:50 -0300

    feat: add statusbar for app

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Groups } from '@screens/Groups'
 import theme from '@theme/index'
 import { Loading } from '@components/Loading'
+import { StatusBar } from 'react-native'
 
 const App = () => {
   const [fontsLoaded] = useFonts({
@@ -13,6 +14,11 @@ const App = () => {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        translucent
+        barStyle={'light-content'}
+        backgroundColor={'transparent'}
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   )

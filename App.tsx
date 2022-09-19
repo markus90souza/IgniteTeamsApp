1ca30d963c435faf7e444c48a67339af30c0dff5
markus90souza <markus90souza@gmail.com>
2022-09-19 12:33:23 -0300

    feat: add custom fonts for app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import { ThemeProvider } from 'styled-components/native'
+import { useFonts } from 'expo-font'
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Groups } from '@screens/Groups'
 import theme from '@theme/index'
+import { ActivityIndicator } from 'react-native'
 
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator size={24} />}
     </ThemeProvider>
   )
 }

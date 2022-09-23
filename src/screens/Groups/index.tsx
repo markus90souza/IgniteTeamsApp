6266d3b8ec808241cff783a77a634c0f7a8eb16f
markus90souza <markus90souza@gmail.com>
2022-09-23 13:06:37 -0300

    feat: create list itens in groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,24 @@
-import { TeamCard } from '@components/Card/TeamCard'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
-
+import { TeamCard } from '@components/Card/TeamCard'
 import { Container } from './styles'
 
 const Groups = () => {
+  const [groups, setGroups] = useState<string[]>(['Team A', 'Team B', 'Team C'])
   return (
     <Container>
       <Header />
-
       <Highlight title={'Turmas'} subTitle={'Jogue com sua turma'} />
 
-      <TeamCard title="Time A" />
-      <TeamCard title="Time B" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => {
+          return <TeamCard title={item} />
+        }}
+      />
     </Container>
   )
 }
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,8 +2,6 @@ import styled from 'styled-components/native'
 
 export const Container = styled.SafeAreaView`
   flex: 1;
-
-  align-items: center;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
-  padding: 0 24px;
+  padding: 24px;
 `

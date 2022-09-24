d24045e5fbc67690e38ec08a9bd81badaff3cd66
markus90souza <markus90souza@gmail.com>
2022-09-24 16:41:46 -0300

    feat: create filter component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,15 +1,20 @@
 /* eslint-disable no-unused-vars */
-
+import { useState } from 'react'
 import { Button } from '@components/Button'
 import { ButtonIcon } from '@components/ButtonIcon'
+import { Filter } from '@components/Filter'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Input } from '@components/Input'
-import { Container, Form } from './styles'
+
+import { Container, Form, HeaderPlayerList, NumberOfPlayers } from './styles'
+import { FlatList } from 'react-native'
 
 type PlayersProps = {}
 
 const Players = () => {
+  const [team, setTeam] = useState('Team A')
+  const [players, setPlayers] = useState('2')
   return (
     <Container>
       <Header showBackButton />
@@ -22,6 +27,21 @@ const Players = () => {
         <Input placeholder="Nome do participante" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
+
+      <HeaderPlayerList>
+        <FlatList
+          data={['Team A', 'Team B']}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderPlayerList>
 
       <Button title="Remover turma" variant="SECONDARY" />
     </Container>
--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components/native'
+import styled, { css } from 'styled-components/native'
 
 export const Container = styled.View`
   flex: 1;
@@ -12,4 +12,19 @@ export const Form = styled.View`
   flex-direction: row;
   justify-content: center;
   background-color: ${({ theme }) => theme.COLORS.GRAY_700};
+`
+
+export const HeaderPlayerList = styled.View`
+  width: 100%;
+  margin: 32px 0 12px;
+  flex-direction: row;
+  align-items: center;
+`
+
+export const NumberOfPlayers = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-size: ${theme.FONT_SIZE.SM}px;
+    color: ${theme.COLORS.GRAY_200};
+  `};
 `

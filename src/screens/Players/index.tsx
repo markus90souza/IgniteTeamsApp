74185d816af1c4bdda6d876022d00ef2e23cee5a
markus90souza <markus90souza@gmail.com>
2022-09-24 17:39:24 -0300

    feat: create playerCard component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,20 +1,23 @@
 /* eslint-disable no-unused-vars */
 import { useState } from 'react'
+import { FlatList } from 'react-native'
+
 import { Button } from '@components/Button'
 import { ButtonIcon } from '@components/ButtonIcon'
 import { Filter } from '@components/Filter'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Input } from '@components/Input'
+import { PlayerCard } from '@components/Card/PlayerCard'
+import { ListEmpty } from '@components/ListEmpty'
 
 import { Container, Form, HeaderPlayerList, NumberOfPlayers } from './styles'
-import { FlatList } from 'react-native'
 
 type PlayersProps = {}
 
 const Players = () => {
   const [team, setTeam] = useState('Team A')
-  const [players, setPlayers] = useState('2')
+  const [players, setPlayers] = useState([])
   return (
     <Container>
       <Header showBackButton />
@@ -31,6 +34,7 @@ const Players = () => {
       <HeaderPlayerList>
         <FlatList
           data={['Team A', 'Team B']}
+          keyExtractor={(i) => i}
           renderItem={({ item }) => (
             <Filter
               title={item}
@@ -42,6 +46,21 @@ const Players = () => {
         />
         <NumberOfPlayers>{players.length}</NumberOfPlayers>
       </HeaderPlayerList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(i) => i}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+        ListEmptyComponent={() => {
+          return <ListEmpty message="Não há nenhum jogar" />
+        }}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
+      />
 
       <Button title="Remover turma" variant="SECONDARY" />
     </Container>
